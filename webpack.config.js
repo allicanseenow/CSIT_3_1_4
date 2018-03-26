68f8858ecf4d391d5bfa30b092da33f444a46c29
Amakawa <firesidecake@gmail.com>
2018-03-26 15:15:35 +1100

    set up node

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, 'Client'),
+  entry: {
+    javascript: './app.js',
+    html: './index.html'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react', 'stage-2'],
+          // stage-2 has already include class-properties
+          // plugins: [
+          //   'transform-class-properties'
+          // ]
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: '[name].[ext]',
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 1000,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          { loader: "file-loader" }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  }
+};
