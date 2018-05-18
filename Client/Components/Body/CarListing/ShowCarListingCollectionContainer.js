2400dfbd50ed5dcee054d5e4e425af8ebd76ccea
Amakawa <firesidecake@gmail.com>
2018-05-18 19:59:21 +1000

    - Made: Accept/Reject booking request page (Component + logic) - Add default input for owners when creating a new car

--- Client/Components/Body/CarListing/ShowCarListingCollectionContainer.js
+++ Client/Components/Body/CarListing/ShowCarListingCollectionContainer.js
@@ -1,5 +1,4 @@
 import React, { Component }                                       from 'react';
-import PropTypes                                                  from 'prop-types';
 import ShowCarListingCollectionComponent                          from "./ShowCarListingCollectionComponent";
 
 export default class ShowCarListingCollectionContainer extends Component {
